f35fb44e5a7e801ba71fa327860cd7625611fa22
jessemazzie <jessemazzie1996@gmail.com>
2024-08-27 23:40:33 -0400

    added test for ge

--- osrs_api/grandexchange.py
+++ osrs_api/grandexchange.py
@@ -1,4 +1,4 @@
-import restadapter
+from . import restadapter
 
 
 class GrandExchange:
---
+++ tests/test_grandexchange.py
@@ -0,0 +1,19 @@
+from osrs_api import grandexchange
+import json
+
+test_cases = [
+    {"name": "Saradomin godsword", "item_id": 11806},
+    {"name": "Rune scimitar", "item_id": 1333},
+    {"name": "Green partyhat", "item_id": 1044},
+]
+
+
+def test_get_item():
+    ge = grandexchange.GrandExchange()
+    for test_case in test_cases:
+        item_name = test_case["name"]
+        item_id = test_case["item_id"]
+
+        item = json.loads(ge.get_item(item_id))["item"]
+        print(item)
+        assert item["name"] == item_name
