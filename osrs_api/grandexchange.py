64135972b343b42e53b821cc65880612bbb1c528
jessemazzie <jessemazzie1996@gmail.com>
2024-08-27 23:20:31 -0400

    added flake8 config, documentation to ge api calls

--- osrs_api/grandexchange.py
+++ osrs_api/grandexchange.py
@@ -5,7 +5,13 @@ class GrandExchange:
     def __init__(self):
         self.base_url = "https://secure.runescape.com/m=itemdb_oldschool/api/"
 
-    def get_category(self, category_id, starting_letter: str = None, page: int = None):
+    # /CATOLOGUE/ endpoints
+    def get_category(
+        self, starting_letter: str = None, page: int = None, category_id: int = 1
+    ):
+        """/api/catalogue/items.json?category=1&alpha={starting_letter}&page={page}
+        Gets a specific category of items from the grand exchange. Optionally accepts a starting letter (of item name) and page number.
+        """
         url = self.base_url + "catalogue/category.json?category=" + str(category_id)
 
         if starting_letter:
@@ -17,10 +23,15 @@ class GrandExchange:
         return restadapter.call_api(url)
 
     def get_item(self, item_id):
+        """/api/catalogue/detail.json?item={item_id}
+        Gets a single item by its item id."""
         url = self.base_url + "catalogue/detail.json?item=" + str(item_id)
         return restadapter.call_api(url)
 
     def get_items(self, starting_letter: str, page: int, category_id: int = 1):
+        """/api/catalogue/items.json?category=1&alpha={starting_letter}&page={page}
+        Gets a list of items from the grand exchange. Optionally accepts a starting letter (of item name) and page number.
+        """
         url = self.base_url + "catalogue/items.json?category=" + str(category_id)
 
         if starting_letter:
